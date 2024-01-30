0329f3127e36095a9f5d502024522fa4af05d3c7
Nandhana <nandhana.rajeev@msds.christuniversity.in>
2024-01-30 20:46:56 +0530

    Update

--- booking/urls.py
+++ booking/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('user_login',views.user_login, name = "user_login"), 
-    path('booking_index',views.booking_index, name = "booking_index"), 
+    path('user_login',views.user_login, name = "user_login"),  
     path('trip',views.trip, name = "trip"),
+    path('booking_index',views.booking_index, name = "booking_index"),
 ]
