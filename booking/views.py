76b844e72507128070c36edae1bc9dc7ef989cf6
George Thomas <george.thomas@msds.christuniversity.in>
2024-02-01 13:14:07 +0530

    update

--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.db import models
+from .models import booking
+
+
 
 # Create your views here.
 
@@ -10,6 +14,14 @@ def login(request):
     return render(request, 'users/login.html')
 
 def booking_index(request):
+    if request.method == 'post':
+        name = request.POST['name']
+        destination = request.POST['destination']
+        activity = request.POST['activity']
+        date = request.POST['date']
+        book = booking(name=name,activity=activity,destination=destination,date=date)
+        book.save()
+        return redirect('trip')
     return render(request, 'booking/booking_index.html')
 
 def user_login(request):
