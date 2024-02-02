73ba26006a7fbe156559b045a2499609ea55b593
Nandhana <nandhana.rajeev@msds.christuniversity.in>
2024-02-02 11:12:03 +0530

    git commit "changes"

--- booking/views.py
+++ booking/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.db import models
 from .models import booking
+from django.contrib.auth.decorators import login_required
 
 
 
@@ -31,9 +32,9 @@ def login(request):
             return render(request, 'users/login.html', {'msg': msg})
     return render(request, 'users/login.html')
 
-
+@login_required(login_url=)
 def booking_index(request):
-    if request.method == 'post':
+    if request.method == 'POST':
         name = request.POST['name']
         destination = request.POST['destination']
         activity = request.POST['activity']
@@ -43,29 +44,6 @@ def booking_index(request):
         return redirect('trip')
     return render(request, 'booking/booking_index.html')
 
-<<<<<<< HEAD
-def user_login(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        print(username, password)
-        user = authenticate(request, username=username, password=password)
-        if user is not None and user.is_active:
-
-            if user.is_superuser:
-                return redirect('cadmin')
-            elif user.is_user:
-                return redirect('booking_index')
-            else:
-                msg = "You are not autherized for this login"
-                return render(request, 'users/login.html', {'msg': msg})
-        else:
-            msg = "Invalid Credentials. Please try again!"
-            return render(request, 'users/login.html', {'msg': msg})
-    return render(request, 'users/login.html')
-    
-=======
->>>>>>> cd3ba2deeee58a55a08a0e3efc05f15ba23fd762
 
 def trip(request):
     return render(request, 'booking/trip.html')
--- to_destination/urls.py
+++ to_destination/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
-    path('', include('booking.urls')),
+    path('booking/', include('booking.urls')),
 ]
