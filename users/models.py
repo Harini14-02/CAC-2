27e87ff954704ffa7a61869962d0171a76eae116
Harini <hsruthi14@gmail.com>
2024-02-05 18:52:49 +0530

    updates

--- users/models.py
+++ users/models.py
@@ -4,10 +4,11 @@ from django.db import models
 
     
 class blog(models.Model):
+    title = models.CharField(blank=False,max_length=500)
     username = models.CharField(blank=False,max_length=50)
     email = models.EmailField(blank=False,max_length=200)
     message = models.TextField(blank=False,max_length=250)
-    image = models.ImageField(blank=False,max_length=250)
+
     
     def _str_(self):
         return str(self.username._str_())
